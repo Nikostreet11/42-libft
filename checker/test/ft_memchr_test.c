9f5cc94591403f3eb0e5834dc1a27909cc14a7b4
Nikostreet11 <nico.pollini@gmail.com>
2024-07-04 16:15:47 +0200

    Add ft_memchr

---
+++ checker/test/ft_memchr_test.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr_test.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nipollin <nipollin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/04 15:54:32 by nipollin          #+#    #+#             */
+/*   Updated: 2024/07/04 16:07:49 by nipollin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "utils.h"
+#include "libft.h"
+
+int	main(int argc, char **argv)
+{
+	size_t	n;
+	
+	if (argc != 4 || ft_strlen(argv[2]) != 1)
+	{
+		printf("Error. Usage: s (string), c (char), n (uint)\n");
+		return (1);
+	}
+	n = atoi(argv[3]);
+	printf("Received: %p\n", ft_memchr(argv[1], *argv[2], n));
+	printf("Expected: %p\n", memchr(argv[1], *argv[2], n));
+	return (0);
+}
