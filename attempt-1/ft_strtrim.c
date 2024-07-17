e21984aa04c9393c91db24de472b08fc94ccb724
Nikostreet11 <nico.pollini@gmail.com>
2024-07-17 11:19:55 +0200

    Add ft_strtrim

---
+++ attempt-1/ft_strtrim.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nipollin <nipollin@student.42firenze.it    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/17 10:05:17 by nipollin          #+#    #+#             */
+/*   Updated: 2024/07/17 11:17:23 by nipollin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*trim;
+	size_t	trim_l;
+	size_t	s1_l;
+	size_t	head;
+	size_t	tail;	
+
+	s1_l = ft_strlen(s1);
+	head = 0;
+	while (head < s1_l && ft_strchr(set, s1[head]))
+	{
+		head++;
+	}
+	tail = s1_l - 1;
+	while (tail >= head && ft_strchr(set, s1[tail]))
+	{
+		tail--;
+	}
+	trim_l = tail - head + 1;
+	trim = (char *) ft_calloc(trim_l + 1, sizeof(char));
+	if (trim == NULL)
+	{
+		return (NULL);
+	}
+	ft_strlcpy(trim, s1 + head, trim_l + 1);
+	return (trim);
+}
---
+++ checker/test/ft_strtrim_test.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim_test.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nipollin <nipollin@student.42firenze.it    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/26 15:09:00 by nipollin          #+#    #+#             */
+/*   Updated: 2024/07/17 10:31:15 by nipollin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "utils.h"
+#include "libft.h"
+
+int	main(int argc, char *argv[])
+{
+	char	*trim;
+	
+	if (argc != 3)
+	{
+		printf("Error. Usage: s1 (string), s2 (string)\n");
+		return (1);
+	}
+	trim = ft_strtrim(argv[1], argv[2]);
+	printf("Received: %s\n", trim);
+	free(trim);
+	return (0);
+}
