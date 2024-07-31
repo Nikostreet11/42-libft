dda663a4fff039db0b9e8f26d487bd4d1d8612c4
Nikostreet11 <nico.pollini@gmail.com>
2024-07-31 18:00:24 +0200

    Add ft_putchar_fd

---
+++ checker/test/ft_putchar_fd_test.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putchar_fd_test.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nipollin <nipollin@student.42firenze.it    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/26 15:09:00 by nipollin          #+#    #+#             */
+/*   Updated: 2024/07/31 13:11:23 by nipollin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "utils.h"
+#include "libft.h"
+
+int	main(int argc, char *argv[])
+{
+	char	c;
+	char	dest;
+	int		fd;
+	
+	if (argc != 2 || ft_strlen(argv[1]) != 1)
+	{
+		printf("Error. Usage: c (char)\n");
+		return (1);
+	}
+	c = *argv[1];
+	fd = open("util/test.txt", O_RDWR);
+	if (fd == -1)
+	{
+		printf("Error.\n");
+		return (2);
+	}
+	ft_putchar_fd(c, fd);
+	lseek(fd, 0, SEEK_SET);
+	read(fd, &dest, 1);
+	printf("Received: %c\n", dest);
+	return (0);
+}
