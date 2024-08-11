ff1f4eed99de42a6e8d5404f09545c930bcb3312
Nikostreet11 <nico.pollini@gmail.com>
2024-08-11 16:41:18 +0200

    Add ft_lstadd_back

---
+++ attempt-1/ft_lstadd_back.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nipollin <nipollin@student.42firenze.it    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/09 12:50:32 by nipollin          #+#    #+#             */
+/*   Updated: 2024/08/11 16:38:44 by nipollin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*tail;
+	
+	if (!(*lst))
+	{
+		*lst = new;
+		return ;
+	}
+	tail = ft_lstlast(*lst);
+	tail->next = new;
+}
