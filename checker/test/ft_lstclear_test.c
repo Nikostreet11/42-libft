5965d8100e848dc1cc9a300d69aafb5ff369aded
Nikostreet11 <nico.pollini@gmail.com>
2024-08-11 17:44:37 +0200

    Add ft_lstclear

---
+++ checker/test/ft_lstclear_test.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear_test.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nipollin <nipollin@student.42firenze.it    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/26 15:09:00 by nipollin          #+#    #+#             */
+/*   Updated: 2024/08/11 17:36:32 by nipollin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "utils.h"
+#include "libft.h"
+
+int	main(int argc, char **argv)
+{
+	t_list	*head;
+	t_list	*node;
+	size_t	idx;
+	
+	if (argc < 1)
+	{
+		printf("Error.\n");
+		return (1);
+	}
+	head = NULL;
+	idx = 0;
+	while (idx + 1 < (size_t) argc)
+	{
+		node = ft_lstnew(ft_strdup(argv[idx + 1]));
+		ft_lstadd_back(&head, node);
+		idx++;
+	}
+	ft_lstclear(&head, free);
+	write(STDOUT_FILENO, "Received: ", 10);
+	ft_write_lst(head);
+	write(STDOUT_FILENO, "\n", 1);
+	return (0);
+}
