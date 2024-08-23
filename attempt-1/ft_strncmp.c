5be96eefb6ffdb9b5162b2a0e62282e54e72f728
Nikostreet11 <nico.pollini@gmail.com>
2024-08-23 12:13:37 +0200

    Fix ft_strncmp bug with extended ascii

--- attempt-1/ft_strncmp.c
+++ attempt-1/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: nipollin <nipollin@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 12:49:05 by nipollin          #+#    #+#             */
-/*   Updated: 2024/08/22 16:00:20 by nipollin         ###   ########.fr       */
+/*   Updated: 2024/08/23 12:10:53 by nipollin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,19 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	idx;
-	int		value;
+	const unsigned char	*us1;
+	const unsigned char	*us2;
+	size_t				idx;
+	int					value;
 
+	us1 = (const unsigned char *) s1;
+	us2 = (const unsigned char *) s2;
 	idx = 0;
 	value = 0;
 	while (idx < n)
 	{
-		value = s1[idx] - s2[idx];
-		if (value || !s1[idx] || !s2[idx])
+		value = us1[idx] - us2[idx];
+		if (value || !us1[idx] || !us2[idx])
 		{
 			return (value);
 		}
