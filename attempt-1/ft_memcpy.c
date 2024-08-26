0161b69beab1de5e3de661468e9f5f17916422f5
Nikostreet11 <nico.pollini@gmail.com>
2024-08-26 12:35:02 +0200

    Fix ft_memcpy and ft_memmove bug with NULL input parameters

--- attempt-1/ft_memcpy.c
+++ attempt-1/ft_memcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nipollin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: nipollin <nipollin@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 15:50:56 by nipollin          #+#    #+#             */
-/*   Updated: 2024/06/20 16:12:09 by nipollin         ###   ########.fr       */
+/*   Updated: 2024/08/26 12:28:50 by nipollin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,10 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char		*d;
 	unsigned const char	*s;
 
+	if (!dest && !src)
+	{
+		return (NULL);
+	}
 	d = dest;
 	s = src;
 	idx = 0;
